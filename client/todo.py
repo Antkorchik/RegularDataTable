a9b829c631deebbfbf3cd3cc6cb1a5ff20ffa2de
Antkorchik <andreyyyyp@gmail.com>
2026-06-29 20:58:06 +0300

    Update todo.py

--- client/todo.py
+++ client/todo.py
@@ -1,15 +1,12 @@
 import sqlite3
 
-# Подключаемся к базе
 conn = sqlite3.connect('auth.db')
 cursor = conn.cursor()
 
-# Сначала проверим существование таблицы
 cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
 tables = cursor.fetchall()
 print("Таблицы в базе:", tables)
 
-# Если таблицы users нет, создаем ее
 if not any('users' in table for table in tables):
     print("Создаем таблицу users...")
     cursor.execute('''
@@ -24,7 +21,6 @@ if not any('users' in table for table in tables):
     conn.commit()
     print("Таблица users создана!")
 
-# Теперь можно выполнять запрос
 cursor.execute("SELECT * FROM users")
 users = cursor.fetchall()
 
